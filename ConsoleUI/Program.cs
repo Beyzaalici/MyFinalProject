ee3d04f6df9e84d3b350730648f0dbf15945fb3d
beyza ALICI <alicibeyza6@gmail.com>
2021-02-14 00:05:11 +0300

    6 şubat ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -5,13 +5,20 @@ using System;
 
 namespace ConsoleUI
 {
+    //Data Transformation object DTOs
     //SOLID
     //Open Closed Princple
     class Program
     {
         static void Main(string[] args)
         {
-            //ProductTest();
+            ProductTest();
+            //IoC
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
@@ -23,9 +30,9 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40, 100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
         }
     }
